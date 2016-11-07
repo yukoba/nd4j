e0210f81a2c257953726987a92aeb33fdbe5328f
raver119 <raver119@gmail.com>
2016-11-07 21:17:26 +0300

    off 6

--- nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/crash/CrashTest.java
+++ nd4j-backends/nd4j-tests/src/test/java/org/nd4j/linalg/crash/CrashTest.java
@@ -128,16 +128,16 @@ public class CrashTest extends BaseNd4jTest {
         float sum = x.sumNumber().floatValue();
 
         // index reduction
-        Nd4j.getExecutioner().exec(new IMax(x), Integer.MAX_VALUE);
+//        Nd4j.getExecutioner().exec(new IMax(x), Integer.MAX_VALUE);
 
         // casual transform
-        Nd4j.getExecutioner().exec(new Sqrt(x, x));
+//        Nd4j.getExecutioner().exec(new Sqrt(x, x));
 
         //  dup
-        INDArray x1 = x.dup(x.ordering());
-        INDArray x2 = x.dup(x.ordering());
-        INDArray x3 = x.dup('c');
-        INDArray x4 = x.dup('f');
+//        INDArray x1 = x.dup(x.ordering());
+//        INDArray x2 = x.dup(x.ordering());
+//        INDArray x3 = x.dup('c');
+//        INDArray x4 = x.dup('f');
 
 
         // vstack && hstack
